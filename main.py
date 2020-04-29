a20d6da966a9a09fbf6488f146f493918ecbb579
Daniel Owens <kyoji@gmail.com>
2020-04-28 23:09:15 -0400

    initial push

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import garbled_bloom_filter as gbf
 import PySimpleGUI as sg
 
 # Turn all debug prints to print in a window
-print = sg.Print
+# print = sg.Print
 
 sg.theme('LightBlue')
 
@@ -22,40 +22,48 @@ p = 0.3 # 0.25 # Fraction of messages to use for Cut and Choose
 a = 0.27 # 0.25 # Probability a 1 is chosen by a player
 
 layout = [ 
-            [sg.Text('Efficient Multi-Party PSI', size=(50,1), justification='center', font=("Helvetic, 20"))],
-            [sg.Text('By Malia Kency and John Owens', font=("Helvetic, 13"))],
+            [sg.Text('Efficient Multi-Party PSI', size=(50,1), justification='center', font=('Segoe UI', 20))],
+            [sg.Text('By Malia Kency and John Owens', font=('Segoe UI', 13))],
             [sg.Text('')],
-            [sg.Text('Constant protocol parameters that will be used:', font=("Helvetic, 12"))],
-            [sg.Listbox(values=['NumPlayers      = Total number of players, P\N{LATIN SUBSCRIPT SMALL LETTER I}',
-                                'PlayerInputSize = Size of the players input sets',
-                                'N_BF               = Length fo Bloom Filter',
-                                'k                      = Number of hash functions to use',
-                                'SecParam (kappa)= Security Paramter',
-                                'N_maxones       = Max number of ones a player is allowed after cut-and-choose',
-                                'p                      = Percentage of total messages to be used for cut-and-choose',
-                                'a                      = Sampling weight of 1s vs. 0s for every P\N{LATIN SUBSCRIPT SMALL LETTER I}'], size=(70,8))],
-            [sg.Text('Parameters that will be calculated:', font=("Helvetic, 12"))],
-            [sg.Listbox(values=['N_OT     = Total number of Random Oblivious Transfer',
-                                'm\N{LATIN SUBSCRIPT SMALL LETTER h}         = The number of 1s a player chooses',
-                                'gamma (\N{GREEK SMALL LETTER GAMMA})          = Verifies the correct relationship between p, k, m\N{LATIN SUBSCRIPT SMALL LETTER h}',
-                                'gammaStar (\N{GREEK SMALL LETTER GAMMA}*)   = Verifies the correct relationship between p, k, N_OT',
-                                'varphi',
-                                '\N{GREEK SMALL LETTER PI}',
-                                'epsilon',
-                                'lambda'], size=(70,8))],
-            [sg.Button('Perform Protocol', font=("Helvetic, 12"))],
-            [sg.Button('Exit', font=("Helvetic, 12"))]
+            [sg.Text('Constant protocol parameters that will be used:', font=('Segoe UI', 12))],
+            [sg.Listbox(
+                values = [
+                    'NumPlayers         = Total number of players, P\N{LATIN SUBSCRIPT SMALL LETTER I}',
+                    'PlayerInputSize    = Size of the players input sets',
+                    'N_BF               = Length fo Bloom Filter',
+                    'k                  = Number of hash functions to use',
+                    'SecParam (kappa)   = Security Paramter',
+                    'N_maxones          = Max number of ones a player is allowed after cut-and-choose',
+                    'p                  = Percentage of total messages to be used for cut-and-choose',
+                    'a                  = Sampling weight of 1s vs. 0s for every P\N{LATIN SUBSCRIPT SMALL LETTER I}'],
+                                
+            size=(70,8), font=('Consolas', 10))],
+            [sg.Text('Parameters that will be calculated:', font=('Segoe UI', 12))],
+            [sg.Listbox(
+                values = [
+                    'N_OT               = Total number of Random Oblivious Transfer',
+                    'm\N{LATIN SUBSCRIPT SMALL LETTER h}                = The number of 1s a player chooses',
+                    'gamma (\N{GREEK SMALL LETTER GAMMA})               = Verifies the correct relationship between p, k, m\N{LATIN SUBSCRIPT SMALL LETTER h}',
+                    'gammaStar (\N{GREEK SMALL LETTER GAMMA}*)              = Verifies the correct relationship between p, k, N_OT'],
+            size=(70,8), font=('Consolas', 10))],
+            [sg.Output(size=(70, 8), font=('Consolas', 10), key='-OUTPUT-')],
+            [sg.Button('Perform Protocol', font=('Segoe UI', 12))],
+            [sg.Button('Exit', font=('Segoe UI', 12))]
          ]
 
-window = sg.Window('Private Set Intersection', layout, default_element_size=(50,1), size=(900,900), grab_anywhere=True, )
+window = sg.Window('Private Set Intersection', layout, default_element_size=(50,1), size=(900,900), grab_anywhere=True )
 
 while True:
     # Read the event that happened and the values dictionary
-    event, values = window.read() 
-    print(event, values)
+    event, values = window.read()
+    # print(event, values)
+
     if event in (None, 'Exit'): 
         break
     if event == 'Perform Protocol':
+        print('test')
+        # print(event, values)
+
         # Initialize the protocol by calculating parameters,
         # creating the players, and generating random inputs
         # Note: at least 1 shared value is guaranteed
