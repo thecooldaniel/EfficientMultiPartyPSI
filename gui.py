da600252307d8869e7e229506c73306d33cdacfa
Maliaeck <Mkency2014@fau.edu>
2020-04-29 20:50:44 -0400

    Updating button functionality

--- gui.py
+++ gui.py
@@ -45,10 +45,10 @@ layout = [
                     size=(70,8), font=('Consolas', 10)),
                 sg.Listbox(
                     values = [
-                        'N_OT               = Total number of Random Oblivious Transfer',
+                        'N_OT              = Total number of Random Oblivious Transfer',
                         'm\N{LATIN SUBSCRIPT SMALL LETTER h}                = The number of 1s a player chooses',
-                        'gamma (\N{GREEK SMALL LETTER GAMMA})               = Verifies the correct relationship between p, k, m\N{LATIN SUBSCRIPT SMALL LETTER h}',
-                        'gammaStar (\N{GREEK SMALL LETTER GAMMA}*)              = Verifies the correct relationship between p, k, N_OT'],
+                        'gamma (\N{GREEK SMALL LETTER GAMMA})         = Verifies the correct relationship between p, k, m\N{LATIN SUBSCRIPT SMALL LETTER h}',
+                        'gammaStar (\N{GREEK SMALL LETTER GAMMA}*)    = Verifies the correct relationship between p, k, N_OT'],
                         size=(70,8), font=('Consolas', 10))
             ],
             [sg.Output(key='-OUTPUT-', size=(300, 20), font=('Consolas', 10))],
@@ -65,9 +65,8 @@ while True:
     if event in (None, 'Exit'): 
         break
     if event == '-RUN-':
-        perform_protocol.Update("Button clicked {} times".format(stepTracker) )
         stepTracker += 1
-        print(stepTracker)
+        perform_protocol.Update("Update Protocol")
         # Initialize the protocol by calculating parameters,
         # creating the players, and generating random inputs
         # Note: at least 1 shared value is guaranteed
@@ -78,43 +77,46 @@ while True:
         print("gammaStar = {}".format(Protocol.params.gammaStar))
         print("\nSimulating players joining protocol. Total: {}".format(Protocol.params.NumPlayers))
         
+    if ((event == '-RUN-') and (stepTracker == 2)):
+            # Perform the random oblivious transfer simulation for P0...Pt
+            print("\nPerforming Random Oblivious Transfer simulation. {} transfers in total:".format(Protocol.params.Not))
+            Protocol.perform_RandomOT()
+            output = Protocol.print_PlayerROTTable()
+            print(output)
+            print("\nCounting each player's \"1s\":")
+            output = Protocol.print_PlayerMessageStats()
+            print(output)
 
-        # Perform the random oblivious transfer simulation for P0...Pt
-        print("\nPerforming Random Oblivious Transfer simulation. {} transfers in total:".format(Protocol.params.Not))
-        Protocol.perform_RandomOT()
-        output = Protocol.print_PlayerROTTable()
-        print(output)
-        print("\nCounting each player's \"1s\":")
-        output = Protocol.print_PlayerMessageStats()
-        print(output)
+    if ((event == '-RUN-') and (stepTracker == 3)):
+            # Perform cut-and-choose simulation for P0...Pt
+            print("\nPerforming Cut and Choose simulation. Size of c: {}. Size of j: {}".format(Protocol.params.C, Protocol.params.Not - Protocol.params.C))
+            Protocol.perform_CutandChoose()
 
-        # Perform cut-and-choose simulation for P0...Pt
-        print("\nPerforming Cut and Choose simulation. Size of c: {}. Size of j: {}".format(Protocol.params.C, Protocol.params.Not - Protocol.params.C))
-        Protocol.perform_CutandChoose()
+    if ((event == '-RUN-') and (stepTracker == 4)):
+            # Create bloom filters for P1...Pt
+            print("\nCreating Bloom Filters. BF length: {}".format(Protocol.params.Nbf))
+            Protocol.create_BloomFilters()
 
-        # Create bloom filters for P1...Pt
-        print("\nCreating Bloom Filters. BF length: {}".format(Protocol.params.Nbf))
-        Protocol.create_BloomFilters()
+     if ((event == '-RUN-') and (stepTracker == 5)):
+            # Create P1...Pt's injective functions
+            print("\nCreating injective functions for every Pi:")
+            output = Protocol.create_InjectiveFunctions()
+            print(output)
 
-        # Create P1...Pt's injective functions
-        print("\nCreating injective functions for every Pi:")
-        output = Protocol.create_InjectiveFunctions()
-        print(output)
+            # Instantiate P0's and P1's rGBF objects
+            print("\nCreating randomized GBF for every Pi")
+            Protocol.create_RandomizedGBFs()
 
-        # Instantiate P0's and P1's rGBF objects
-        print("\nCreating randomized GBF for every Pi")
-        Protocol.create_RandomizedGBFs()
+            # P0 performs XOR summation on its own j_messages[injective_func] where bit=1
+            # P1 performs XOR summation on all P1...Pt's j_messages[injective_func] where bit = P1...Pt's choice
+            Protocol.perform_XORsummation()
 
-        # P0 performs XOR summation on its own j_messages[injective_func] where bit=1
-        # P1 performs XOR summation on all P1...Pt's j_messages[injective_func] where bit = P1...Pt's choice
-        Protocol.perform_XORsummation()
+            # P0 calculates summary values for all elements of its input set
+            # P1 calculates summary values for all elements of its input set (Every P1...Pt input values)
+            Protocol.perform_SummaryValues()
 
-        # P0 calculates summary values for all elements of its input set
-        # P1 calculates summary values for all elements of its input set (Every P1...Pt input values)
-        Protocol.perform_SummaryValues()
-
-        # P1 receives P0s summary values, compares them to its own
-        # Intersections are recorded and output
-        output = Protocol.perform_Output()
-        print(output)
+            # P1 receives P0s summary values, compares them to its own
+            # Intersections are recorded and output
+            output = Protocol.perform_Output()
+            print(output)
 window.close()
